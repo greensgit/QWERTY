9dc3ffb73b4dfbef71ddf320a1bfb48a541efce1
greensgit <rajgreensomr@gmail.com>
2024-09-21 10:13:24 +0530

    new

--- src/org/dem/tes/Dummy.java
+++ src/org/dem/tes/Dummy.java
@@ -7,4 +7,8 @@ public static void main(String[] args) {
 	System.out.println("This is demo");
 	System.out.println("This is yuvaraj branch");
 }
+
+private void addToKart() {
+	System.out.println("This is anbarasu working on add to kart");
+}
 }
